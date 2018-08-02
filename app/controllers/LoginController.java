c05f4fd9b6fcea19d3fa69a3a04816d2501dddaa
Johannes Schimmer <joschimm@mail.uni-mannheim.de>
2018-08-02 14:03:13 +0200

    added registration and login routing

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -18,31 +18,7 @@ import services.DatabaseService2;
 public class LoginController extends Controller {
     
     
-    /**
-     * Store task method: Gets JSON;
-     * parses the necessary data to create a new Task object and stores it in the DB with a generated ID
-     * responses with JSON {"successful" : true} if the storage went good or {"successful" : false} if not
-     * Tested with POSTMAN:
-     * Example request: 
-     * POST http://localhost:9000/recording/task Headers Content-Type application/json Body {"timeSpent" : double, "date" : String, "notes" : String, "projectId" : int, "employeeId" : int} RESPONSE Body {"successful" : boolean}
-     */
-    public Result storeTask() {
-        JsonNode json = request().body().asJson();
-        String dateString = json.findPath("date").textValue();
-        String notes = json.findPath("notes").textValue();
-        Double timeSpent = json.findPath("timeSpent").doubleValue();
-        Integer projectId = json.findPath("projectId").intValue();
-        Integer employeeId = json.findPath("employeeId").intValue();
 
-        if (timeSpent == null || dateString == null || notes == null || projectId == null || employeeId == null) {
-            return badRequest("Missing parameter [time], [date], [notes], [projectId] or [employeeId]");
-        } else {
-            DatabaseService2.insertNewTask(timeSpent, dateString, notes, projectId, employeeId);
-            ObjectNode result = Json.newObject();
-            result.put("successful", true);
-            return ok(result);
-        }
-    }
 
     
     /**
